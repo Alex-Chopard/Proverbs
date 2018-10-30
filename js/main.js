e6c271cae47711f40127bdb6d61c673add745b8f
Alex Chopard <alex.chopard1998@gmail.com>
2018-10-30 21:19:34 +0100

    Add promise with try catch in methods who use fetch

--- js/main.js
+++ js/main.js
@@ -8,32 +8,60 @@ new Alba({
 		newValue: ''
 	},
 	methods:{
-		async deleteProverb (el, id) {
-			console.log('dellllllllllllllllllllet', el, id)
-			if (id) {
-				await fetch(`service.php?action=delete&id=${id}`, {
-					credentials: 'same-origin',
+		deleteProverb (el, id) {
+			if (id !== null && id !== undefined) {
+				return new Promise(async resolve => {
+					try {
+						await fetch(`service.php?action=delete&id=${id}`, {
+							credentials: 'same-origin',
+						})
+
+						el.loadList(el)
+						resolve(true)
+					} catch (error) {
+						console.error('[main:methods.deleteProverb]')
+						resolve(false)
+					}
 				})
-				el.loadList(el)
 			}
 		},
-		async addProverb (el) {
+		addProverb (el) {
 			if (el.data.newValue && el.data.newValue.length > 0) {
-				await fetch(`service.php?action=add&value=${el.data.newValue}`, {
-					credentials: 'same-origin',
+				return new Promise(async resolve => {
+					try {
+						await fetch(`service.php?action=add&value=${el.data.newValue}`, {
+							credentials: 'same-origin',
+						})
+
+						el.data.newValue = ''
+						el.loadList(el)
+
+						resolve(true)
+					} catch (error) {
+						console.error('[main:methods.addProverb]')
+						resolve(false)
+					}
 				})
-				el.data.newValue = ''
-				el.loadList(el)
 			}
 		},
-		async loadList (el) {
-			const response = await fetch('service.php?action=list', {
-				credentials: 'same-origin',
+		loadList (el) {
+			return new Promise(async resolve => {
+				try {
+					const response = await fetch('service.php?action=list', {
+						credentials: 'same-origin',
+					})
+					
+					if (response) {
+						el.data.proverbs = await response.json()
+						resolve(true)
+					}
+
+					resolve(false)
+				} catch (error) {
+					console.error('[main:methods.deleteProverb]')
+					resolve(false)
+				}
 			})
-			
-			if (response) {
-				el.data.proverbs = await response.json()
-			}
 		}
 	}
 })
